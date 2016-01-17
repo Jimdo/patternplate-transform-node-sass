db1f45905f77d1607d590583c06c7697111f8470
Karsten Buckstegge <k.buckstegge@gmail.com>
2016-01-17 16:12:51 +0100

    teach demos to resolve dependencies

--- source/application/patternplate-server/transforms/sass/index.js
+++ source/application/patternplate-server/transforms/sass/index.js
@@ -3,8 +3,6 @@ import {render} from 'node-sass';
 
 function renderSass(data, options) {
   return new Promise((resolve, reject) => {
-    console.log('-------------->');
-    console.log(data.split('\n').map(line => line.replace(/\s/g, '.')));
     render({
       ...options,
       data
@@ -23,18 +21,42 @@ export default function(application) {
     // file.source
     // file.demoBuffer
     // file.demoSource
+    let fileDependencies = file.dependencies;
 
     let rendered = await renderSass(file.buffer.toString('utf-8'), {
       indentedSyntax: true,
-      includePaths:  []
+      includePaths: [dirname(demo.path)]
     });
 
     file.rendered = rendered.css;
 
     if (demo) {
-      let renderedDemo = await renderSass(demo.buffer.toString('utf-8'), {
+      // resolve dependencies
+      let demoBuffer = demo.buffer.toString('utf-8');
+
+      let demoResolved = demoBuffer.replace(/@import(?:.+?)["|'](.*)["|']/g, function(match, name) {
+        if (name === 'Pattern') {
+          // special case `Pattern`, just for demo
+          return match.replace(name, './index.sass');
+        } else if (name in fileDependencies) {
+          // check if name is found in pattern dependencies
+          let dependency = fileDependencies[name];
+          return match.replace(name, dependency.path);
+        } else if (name.indexOf('npm://') === 0) {
+          // special case `npm://`, for npm dependencies
+          try {
+            return match.replace(name, require.resolve(name.split('npm://')[1]));
+          } catch (err) {
+            application.log.error(`Could not resolve npm dependency: ${name}`);
+          }
+        }
+
+        throw new Error(`Unknown dependency found in ${demo.path}: ${name}`);
+      }) || buffer;
+
+      let renderedDemo = await renderSass(demoResolved, {
         indentedSyntax: true,
-        includePaths:  [dirname(demo.path)]
+        includePaths: [dirname(demo.path)]
       });
       file.demoBuffer = renderedDemo.css;
       file.demoSource = demo.source;
